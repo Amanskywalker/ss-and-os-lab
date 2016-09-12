e9286520dfe821db63005caa19621a307199bd39
Aman Skywalker <ak47su30@gamil.com>
2016-09-12 10:37:49 +0530

    modified:   3.sh

--- 3.sh
+++ 3.sh
@@ -1,3 +1,4 @@
+# program to check the files permissions
 #!/bin/bash
 
 #echo $1
@@ -5,6 +6,10 @@
 
 f1=$1
 f2=$2
+
+if [ $# -ne 2 ]
+  then echo "Usages sh 3.sh <file1> <file2>" ; exit
+fi
 
 if [ ! -e $1 ]
   then echo $1."file does NOT exist"
@@ -17,8 +22,8 @@ b=`ls -l $f2 | cut -c1-10`
 
 #echo $a"\n"$b;
 
-if [ a==b ];then
-  echo "Files permission "$a
+if [ a==b ]
+  then echo "Files permission "$a
 else
   echo "$f1 permissions: $a \n $f2 permissions: $b"
 fi
