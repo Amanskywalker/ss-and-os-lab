9e90337b55395124740a55da155744b50f939828
Aman Skywalker <ak47su30@gamil.com>
2016-09-19 09:19:45 +0530

    added program 9b
     	new file:   9b
     	new file:   9b.c

---
+++ 9b.c
@@ -0,0 +1,29 @@
+#include  <stdio.h>
+#include  <string.h>
+#include  <sys/types.h>
+#include  <unistd.h>
+
+int main(int argc, char const *argv[])
+{
+     pid_t  pid,child_id;
+     pid = fork();
+     if (pid<0)
+     {
+       printf("ERROR forking \n");
+       return 0;
+     }
+     else if(pid==0)
+     {
+        printf("\nChild process i'd : %d",getpid());
+        printf("\nParent process i'd : %d",getppid());
+        return 0;
+     }
+     else
+     {
+       wait(1);
+       printf("\n\nThis is Parent process\n");
+       printf("\nParent process i'd : %d",getpid());
+       printf("\nChild process i'd : %d\n",pid);
+     }
+     return 0;
+}
