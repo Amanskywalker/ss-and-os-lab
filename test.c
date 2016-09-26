5be90bb38fe3004b34769ed12efe10b8e58cfb50
Aman Skywalker <ak47su30@gamil.com>
2016-09-26 10:10:17 +0530

    added new lab program 1b  	new file:   1b.l   test file to test the working of the counter  	new file:   test.c

---
+++ test.c
@@ -0,0 +1,15 @@
+//test.c
+
+// this is a commented program to check the working of 1b.l program
+int a=0;
+
+/*
+this is multi line comment to check the working.
+*/
+
+int main(int argc, char const *argv[])
+{
+  int a=0,b=0;
+  printf("a=%d,b=%d",a,b);
+  return 0;
+}
